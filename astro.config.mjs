59abf38ac8eb96126a6d4fd82be24de8450e8a3e
JonasLillebo <lillebohaugen@gmail.com>
2026-05-25 00:17:52 +0200

    noExternal: ["@threetides/bekk"]

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react"
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    ssr: {
+    resolve: {
       noExternal: ["@threetides/bekk"]
     },
     css: {
